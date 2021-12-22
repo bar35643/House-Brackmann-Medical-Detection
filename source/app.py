e1dca160b59325f10a15ff7120f6266653a53054
bar35643 <raphael.baumann@st.oth-regensburg.de>
2021-12-22 13:54:09 +0100

    Added Argparse and Detection

--- source/app.py
+++ source/app.py
@@ -1,11 +1,13 @@
 import os
 import os.path
 import uuid
+import logging
 from pathlib import Path
 from typing import List
 import uvicorn
 import asyncio
 import datetime
+import argparse
 
 
 import threading
@@ -16,7 +18,14 @@ from fastapi.responses import HTMLResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from utils.general import check_requirements
+
+import detect
+from utils.argparse_utils import restricted_val_split, SmartFormatter
+from utils.general import check_requirements, set_logging, OptArgs
+
+
+PREFIX = "app: "
+LOGGING_STATE = logging.INFO #logging.DEBUG
 
 templates = Jinja2Templates(directory="static/templates")
 app = FastAPI()
@@ -62,6 +71,9 @@ def get_upload(request: Request):
 
 
 
+
+
+
 @app.post("/api/upload/")
 async def post_upload(files: List[UploadFile] = File(...)):
     with scheduler_lock:
@@ -81,6 +93,20 @@ async def post_upload(files: List[UploadFile] = File(...)):
 
         #Do Operation
         print(workspace)
+
+
+        #try:
+        detect.run(weights="models",
+                    source=workspace,
+                    imgsz=640,
+                    batch_size=4,
+                    device="cpu",
+                    half=False,
+                    function_selector="all")
+        #except:
+        #    ret_val = None
+
+
         data = {
             "name": "name",
             "result": "TODO list Result",
@@ -91,6 +117,39 @@ async def post_upload(files: List[UploadFile] = File(...)):
     return data
 
 
+
+
+
+
+
+
+def parse_opt():
+    """
+    TODO
+    Check internet connectivity
+    """
+    parser = argparse.ArgumentParser(formatter_class=SmartFormatter)
+    parser.add_argument("--ip", type=str, default="127.0.0.1",
+                        help="corresponding IP")
+    parser.add_argument("--port", type=int, default=8000,
+                        help="corresponding Port")
+
+    parser.add_argument("--device", default="cpu",
+                        help="Device for Processing gpu or cpu")
+    parser.add_argument("--reload", action="store_true",
+                        help="Reload if Files Changes")
+    parser.add_argument("--workers", type=int, default=4,
+                        help="Number of worker processes")
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    check_requirements()
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True, workers=4)
+    opt_args = vars(parse_opt())
+    OptArgs.instance()(opt_args)
+
+    set_logging(LOGGING_STATE, PREFIX)
+
+    uvicorn.run("app:app",
+                host=OptArgs.instance().get_arg_from_key("ip"),
+                port=OptArgs.instance().get_arg_from_key("port"),
+                reload=OptArgs.instance().get_arg_from_key("reload"),
+                workers=OptArgs.instance().get_arg_from_key("workers"))
